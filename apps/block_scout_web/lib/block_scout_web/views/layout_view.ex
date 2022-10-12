224078f2cbd3eb76b02fe3bfe6c0cb792dfd9cc0
topdefi <94203269+topdefi@users.noreply.github.com>
2022-10-12 08:49:21 -0700

    Secryptify

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -9,27 +9,13 @@ defmodule BlockScoutWeb.LayoutView do
 
   @default_other_networks [
     %{
-      title: "POA",
-      url: "https://blockscout.com/poa/core"
+      title: "Mainnet SECRYPT",
+      url: "https://explorer-mainnet.secrypt.tech"
     },
     %{
-      title: "Sokol",
-      url: "https://blockscout.com/poa/sokol",
+      title: "Testnet SECRYPT",
+      url: "https://explorer-testnet.secrypt.tech",
       test_net?: true
-    },
-    %{
-      title: "Gnosis Chain",
-      url: "https://blockscout.com/xdai/mainnet"
-    },
-    %{
-      title: "Ethereum Classic",
-      url: "https://blockscout.com/etc/mainnet",
-      other?: true
-    },
-    %{
-      title: "RSK",
-      url: "https://blockscout.com/rsk/mainnet",
-      other?: true
     }
   ]
 
--- config/runtime.exs
+++ config/runtime.exs
@@ -30,9 +30,9 @@ indexer_empty_blocks_sanitizer_batch_size =
 config :indexer, Indexer.Fetcher.EmptyBlocksSanitizer, batch_size: indexer_empty_blocks_sanitizer_batch_size
 
 config :block_scout_web, :footer,
-  chat_link: System.get_env("FOOTER_CHAT_LINK", "https://discord.gg/blockscout"),
-  forum_link: System.get_env("FOOTER_FORUM_LINK", "https://forum.poa.network/c/blockscout"),
-  github_link: System.get_env("FOOTER_GITHUB_LINK", "https://github.com/blockscout/blockscout")
+  chat_link: System.get_env("FOOTER_CHAT_LINK", "https://discord.gg/secrypt"),
+  forum_link: System.get_env("FOOTER_FORUM_LINK", "https://twitter.com/secrypt"),
+  github_link: System.get_env("FOOTER_GITHUB_LINK", "https://github.com/SECRYPT-2022/explorer")
 
 ######################
 ### BlockScout Web ###
